c5df5bd75b6a8ced69cf03cc1c7286960fad81d4
dn-rodr <dn.rod@outlook.com>
2023-01-29 17:02:10 -0600

    Rough start.

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+const src = 'src'
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy(src + '/assets/')
+  eleventyConfig.addPassthroughCopy(src + '/scripts/')
+  return {
+    dir: {
+      input: src,
+      output: 'dist'
+    }
+  }
+}
