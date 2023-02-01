5f2de45df97c114725c5bdcc56eb0a17bf8081d9
dn0r <dn.rod@outlook.com>
2023-02-01 00:10:59 -0600

    Prettier formatting edit

--- src/scripts/contentPopulater.js
+++ src/scripts/contentPopulater.js
@@ -1,12 +1,12 @@
-import dataset from "../data/dataset.json" assert {type: "json"}
+import dataset from "../data/dataset.json" assert { type: "json" };
 
-const labReports = dataset.labReports
+const labReports = dataset.labReports;
 
-var parent = document.getElementsByTagName('span')[0]
+var parent = document.getElementsByTagName("span")[0];
 
 for (var n in labReports) {
-  let link = document.createElement('a')  
-  link.href = labReports[n].dir
-  link.innerText = labReports[n].title
-  parent.appendChild(link)
+  let link = document.createElement("a");
+  link.href = labReports[n].dir;
+  link.innerText = labReports[n].title;
+  parent.appendChild(link);
 }
